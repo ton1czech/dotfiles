b1fc4e391b9106f6925891459f783afd0cb08d36
ton1czech <tonyasek007@gmail.com>
2021-04-29 01:22:12 +0200

    👌 IMPROVE: removed omf and using one config file

---
+++ .config/fish/config.fish
@@ -0,0 +1,136 @@
+########
+# fish #
+########
+
+### ADDING TO THE PATH ###
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_paths
+
+### EXPORT ###
+set fish_greeting                                 # Remove fish hello message
+set TERM "xterm-256color"                         # Sets the terminal type
+set EDITOR "vim"								  # $EDITOR use Vim
+
+### SET MANPAGER ###
+set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
+
+### SET VI MODE ###
+function fish_user_key_bindings
+    fish_vi_key_bindings
+end
+
+### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
+set fish_color_normal brcyan
+set fish_color_autosuggestion '#7d7d7d'
+set fish_color_command brcyan
+set fish_color_error '#ff6c6b'
+set fish_color_param brcyan
+
+### ALIASES ###
+# navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+# Changing "ls" to "exa"
+alias ls='exa -lah --color=always --group-directories-first'
+alias lt='exa -aT --color=always --group-directories-first'
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+
+## get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+
+## get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3'
+alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
+
+# switch between shells
+alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
+alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Now log out.'"
+alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
+
+### ZASHIMI STYLE ###
+function fish_prompt
+  set -l last_status $status
+  set -l cyan (set_color -o cyan)
+  set -l yellow (set_color -o yellow)
+  set -g red (set_color -o red)
+  set -g blue (set_color -o blue)
+  set -l green (set_color -o green)
+  set -g normal (set_color normal)
+
+  set -l ahead (_git_ahead)
+  set -g whitespace ' '
+
+  if test $last_status = 0
+    set initial_indicator "$green◆"
+    set status_indicator "$normal❯$cyan❯$green❯"
+  else
+    set initial_indicator "$red✖ $last_status"
+    set status_indicator "$red❯$red❯$red❯"
+  end
+  set -l cwd $cyan(basename (prompt_pwd))
+
+  if [ (_git_branch_name) ]
+
+    if test (_git_branch_name) = 'master'
+      set -l git_branch (_git_branch_name)
+      set git_info "$normal git:($red$git_branch$normal)"
+    else
+      set -l git_branch (_git_branch_name)
+      set git_info "$normal git:($blue$git_branch$normal)"
+    end
+
+    if [ (_is_git_dirty) ]
+      set -l dirty "$yellow ✗"
+      set git_info "$git_info$dirty"
+    end
+  end
+
+  # Notify if a command took more than 5 minutes
+  if [ "$CMD_DURATION" -gt 300000 ]
+    echo The last command took (math "$CMD_DURATION/1000") seconds.
+  end
+
+  echo -n -s $initial_indicator $whitespace $cwd $git_info $whitespace $ahead $status_indicator $whitespace
+end
+
+function _git_ahead
+  set -l commits (command git rev-list --left-right '@{upstream}...HEAD' ^/dev/null)
+  if [ $status != 0 ]
+    return
+  end
+  set -l behind (count (for arg in $commits; echo $arg; end | grep '^<'))
+  set -l ahead  (count (for arg in $commits; echo $arg; end | grep -v '^<'))
+  switch "$ahead $behind"
+    case ''     # no upstream
+    case '0 0'  # equal to upstream
+      return
+    case '* 0'  # ahead of upstream
+      echo "$blue↑$normal_c$ahead$whitespace"
+    case '0 *'  # behind upstream
+      echo "$red↓$normal_c$behind$whitespace"
+    case '*'    # diverged from upstream
+      echo "$blue↑$normal$ahead $red↓$normal_c$behind$whitespace"
+  end
+end
+
+function _git_branch_name
+  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+end
+
+function _is_git_dirty
+  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+end
