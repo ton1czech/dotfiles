ebe09e7a26c8ac0d28f39d586156235a4b7755e2
ton1czech <tonyasek007@gmail.com>
2021-04-29 08:20:18 +0200

    👌 IMPROVE: header changes

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -5,8 +5,10 @@ config.set('content.cookies.accept', 'all', 'devtools://*')
 
 config.set('content.headers.accept_language', '', 'https://matchmaker.krunker.io/*')
 config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}', 'https://web.whatsapp.com/')
-config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version} Edg/{upstream_browser_version}', 'https://accounts.google.com/*')
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0', 'https://accounts.google.com/*')
 config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99 Safari/537.36', 'https://*.slack.com/*')
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0', 'https://docs.google.com/*')
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:71.0) Gecko/20100101 Firefox/71.0', 'https://drive.google.com/*')
 
 config.set('content.images', True, 'chrome-devtools://*')
 config.set('content.images', True, 'devtools://*')
@@ -20,4 +22,6 @@ config.set('content.notifications.enabled', False, 'https://www.youtube.com')
 
 c.downloads.location.directory = '/home/ton1czech/Downloads'
 
-config.set("colors.webpage.darkmode.enabled", True)
+c.aliases = {'q': 'quit', 'w': 'session-save', 'wq': 'quit --save'}
+
+#config.set("colors.webpage.darkmode.enabled", True)
