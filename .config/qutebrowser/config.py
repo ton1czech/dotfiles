92d67dc04f7559e0f0778219853b926f09c9dc79
ton1czech <tonyasek007@gmail.com>
2021-05-22 19:10:39 +0200

    👌IMPROVE: custom css

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -31,11 +31,7 @@ c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}',
                        'wp': 'https://wikipedia.com/wiki/{}',
                        'rd': 'https://reddit.com/search/?q={}'}
 
-config.bind(',ap', 'config-cycle content.user_stylesheets ~/Documents/solarized-everything-css/css/apprentice/apprentice-all-sites.css ""')
-config.bind(',dc', 'config-cycle content.user_stylesheets ~/Documents/solarized-everything-css/css/darculized/darculized-all-sites.css ""')
-config.bind(',gr', 'config-cycle content.user_stylesheets ~/Documents/solarized-everything-css/css/gruvbox/gruvbox-all-sites.css ""')
-config.bind(',sd', 'config-cycle content.user_stylesheets ~/Documents/solarized-everything-css/css/solarized-dark/solarized-dark-all-sites.css ""')
-config.bind(',sl', 'config-cycle content.user_stylesheets ~/Documents/solarized-everything-css/css/solarized-light/solarized-light-all-sites.css ""')
+config.bind(',dr', 'config-cycle content.user_stylesheets ~/.config/qutebrowser/styles/dracula.css ""')
 
 def blood(c, options = {}):
     palette = {
